99e5c8541b984f51c8641bff569bb736c133f625
gemiller100 <geoffrey.e.miller.isu@gmail.com>
2013-10-30 15:20:15 -0500

    new fix for find_perfect

--- Assignment2_Q1.py
+++ Assignment2_Q1.py
@@ -1,8 +1,8 @@
-from math import sqrt ceil
+import math
 # Takes an integer and prints of wether it is a perfect number or not
 def find_perfect(perfect):
 	sum = 1
-	top = ceil(sqrt(perfect))
+	top = math.ceil(math.sqrt(perfect))
 	for i in range(2,int(top)):
 		if(i%perfect==0):
 			sum+= i + perfect/i
