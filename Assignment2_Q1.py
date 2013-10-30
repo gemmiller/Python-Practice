0edcf4153b288eb5501845e6f5bf90de25a43743
gemiller100 <geoffrey.e.miller.isu@gmail.com>
2013-10-30 15:55:05 -0500

    fix to handle squares correctly

--- Assignment2_Q1.py
+++ Assignment2_Q1.py
@@ -2,7 +2,7 @@ import math
 # Takes an integer and prints of wether it is a perfect number or not
 def find_perfect(perfect):
 	sum = 1
-	top = math.ceil(math.sqrt(perfect))
+	top = math.ceil(math.sqrt(perfect)) + 1
 	for i in range(2,int(top)):
 		if(perfect%i==0):
 			sum+= i + perfect/i
