d05b5cd7124406c2c483fe560ea1d9cdfd039160
Geoffrey Miller <gemiller@iastate.edu>
2013-10-31 07:50:34 -0500

    fixed to remove out of bounds error

--- Assignment2_Q3.py
+++ Assignment2_Q3.py
@@ -1,9 +1,9 @@
 # takes in a list of integers and displays a barchart of it
 def display_barchart(list):
 	for i in list:
-		for j in range(list[i]):
-			print "*", end = ""
+		for j in range(i):
+			print "*", 
 		print ""
 
-list = [1,2,3,4,5,6]
+list = [6,5,4,3,2,1,2,3,4,5,6]
 display_barchart(list)
